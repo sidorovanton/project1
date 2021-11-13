5fe1667943e512481e94c0b393edfb5a7ce66d52
Anton <sidorovanton007@gmail.com>
2021-11-14 01:18:01 +0300

    final

---
+++ script.js
@@ -0,0 +1,16 @@
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?","");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastFilm = prompt("Какой последний фильм смотрели?"),
+    raiting = prompt("Какую оценку поставите фильму ?");
+  
+personalMovieDB.movie = {
+    [lastFilm]: raiting
+};
+
