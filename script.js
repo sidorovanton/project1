4750405b25246f004a3afe429fede52fa971842b
Anton <sidorovanton007@gmail.com>
2021-11-15 00:03:05 +0300

    homework complete

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?","");
-let personalMovieDB = {
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
     count: numberOfFilms,
-    movie: {},
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-let lastFilm = prompt("Какой последний фильм смотрели?"),
-    raiting = prompt("Какую оценку поставите фильму ?");
-  
-personalMovieDB.movie[lastFilm] = raiting;
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
